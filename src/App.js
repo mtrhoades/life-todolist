6edbb2a8d0c65b43db06c672c5b562ef878c9a63
root <mtrhoades0727@gmail.com>
2025-05-20 16:31:50 -1000

    To Do list started and styled

--- src/App.js
+++ src/App.js
@@ -1,8 +1,73 @@
 import './App.css';
+
 function App() {
   return (
     <div className="App">
-      
+      <h1>To Do List:</h1>
+
+      <div className="section">
+        <h2>Personal</h2>
+        <ul>
+          <li>Pay off credit cards starting with Chase - 5/23/25!</li>
+          <li>Make better personal budget</li>
+          <li>Switch over health and dental insurance to Ariel's work insurance</li>
+          <li>Download software for linux system for security - (like Find My Device)</li>
+          <li>Buy Ethereum, once order for $500 worth of ADA clears at $0.80/coin</li>
+        </ul>
+      </div>
+
+      <div className="section">
+        <h2>Wife/Kids</h2>
+        <ul>
+          <li>Potty train Sylas!</li>
+          <li>Get Okra for Ariel and make okra water for smooth delivery</li>
+          <li>Make Ariel personal budget, and joint budget</li>
+          <li>Put together hospital bag for baby delivery!</li>
+          <li>Create investment schedule for Ariel and I to automate</li>
+        </ul>
+      </div>
+
+      <div className="section">
+        <h2>At Home</h2>
+        <ul>
+          <li>Put together dresser</li>
+          <li>Setup new baby stuff</li>
+          <li>Go through garage and house - sell items on facebook marketplace</li>
+          <li>Clean front patio, make it nice to sit outside!</li>
+          <li>Fix leaning bookshelf in bedroom</li>
+          <li>Setup cameras around house and backyard</li>
+          <li>Get security door for garage side door</li>
+          <li>Get mesh screen for dryer vent outside</li>
+          <li>Make copies of house keys</li>
+        </ul>
+      </div>
+
+      <div className="section">
+        <h2>King Progress IT</h2>
+        <ul>
+          <li>Create W&K monthly maintenance checklist</li>
+          <li>Organize W&K work/initiatives - consult Ian</li>
+        </ul>
+      </div>
+
+      <div className="section">
+        <h2>Rhoades Tech IT</h2>
+        <ul>
+          <li>Look into Ulta Job - learn DIA circuitry</li>
+          <li>Learn how to terminate a female RJ-45 face plate</li>
+          <li>Setup home lab - server, etc.</li>
+          <li>Create a tax schedule for paying taxes throughout the year - get G.E. License?!</li>
+        </ul>
+      </div>
+
+      <div className="section">
+        <h2>Web Development</h2>
+        <ul>
+          <li>Email Johnny a proposal for his catering website</li>
+          <li>Complete Deborah's art website!</li>
+          <li>Spruce up Asher's website, and finish his Google Business Profile</li>
+        </ul>
+      </div>
     </div>
   );
 }
